b30996f4f7a115440ca933308a1f33a9ed1153e2
Benoît Morgan <bmorgan@laas.fr>
2014-07-04 14:50:52 +0200

    Ajout de stdint.h pour qu'on s'embête pas a faire des bitfields

---
+++ sources/include/base/stdint.h
@@ -0,0 +1,14 @@
+#ifndef __STDINT_H
+#define __STDINT_H
+
+typedef unsigned char uint8_t;
+typedef short unsigned int uint16_t;
+typedef unsigned int uint32_t;
+typedef long long unsigned int uint64_t;
+
+typedef char int8_t;
+typedef short int int16_t;
+typedef int int32_t;
+typedef long long int int64_t;
+
+#endif /* __STDINT_H */
